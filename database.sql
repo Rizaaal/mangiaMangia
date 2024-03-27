eefd0c8215174f352748f3441151cb1b0a4ec739
Rita <rita.ciaramitaro@stevejobs.academy>
2024-03-27 11:12:40 +0100

    fix: change 'cucina' column in 'sezione' to the menuRistorante database table

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 create table menuRistorante (
   id int(11) NOT NULL AUTO_INCREMENT,
   nome varchar(255),
-  cucina varchar(255),
+  sezione varchar(255),
   prezzo float(10, 2) UNSIGNED,
   img varchar(255),
   PRIMARY KEY (id),
