1765a5269d3e43d099400f797f4e1642b60be1d7
Gabriele Di Grazia <gabrii539@gmail.com>
2024-04-03 20:20:44 +0200

    feat: navbar to PageCarrello

--- src/Components/pageCarrello.js
+++ src/Components/pageCarrello.js
@@ -1,6 +1,9 @@
-export default function pageCarrello() {
+import Navbar from './Navbar';
+
+export default function PageCarrello() {
   return (
     <>
+      <Navbar />
       <h1>Carrello</h1>
     </>
   );
