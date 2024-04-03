396d3f4550ea6ef386ca76980a40b6d1caa964fb
Gabriele Di Grazia <gabrii539@gmail.com>
2024-04-03 19:53:19 +0200

    feat: added link to cart in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,34 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">mangiaMangia</span>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        mangiaMangia
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Carrello
+            </a>
+          </li>
+          {/* <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li> */}
+        </ul>
       </div>
     </nav>
   );
