9c175f3a1150b860617b06162d1cc69d7afa750d
Gabriele Di Grazia <gabrii539@gmail.com>
2024-04-03 12:47:12 +0200

    feat: created cart context

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import CartContext from './CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CartContext>
+      <App />
+    </CartContext>
   </React.StrictMode>
 );
 
