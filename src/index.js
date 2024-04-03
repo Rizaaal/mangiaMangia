3075472a63469d7810795628426114dd6db5f06b
Gabriele Di Grazia <gabrii539@gmail.com>
2024-04-03 17:32:23 +0200

    feat: added context provider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import CartContext from './CartContext';
+import ContextProvider from './Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CartContext>
+    <ContextProvider>
       <App />
-    </CartContext>
+    </ContextProvider>
   </React.StrictMode>
 );
 
