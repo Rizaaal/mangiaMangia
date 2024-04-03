bf1292fcfccc5929e2a1cedd616a91f806ae6c43
Gabriele Di Grazia <gabrii539@gmail.com>
2024-04-03 12:47:12 +0200

    feat: created cart context

---
+++ src/CartContext.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext([]);
+
+export default function CartContextProvider({children}){
+  const [cart, setCart] = useState([]);
+
+  return ( 
+    <CartContext.Provider value={{cart, setCart}}>
+      {children}
+    </CartContext.Provider>
+  )
+}
