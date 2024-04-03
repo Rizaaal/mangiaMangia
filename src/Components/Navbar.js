5f711fa1110a5f313ad278f847a6ad7513fe91e5
Gabriele Di Grazia <gabrii539@gmail.com>
2024-04-03 20:19:59 +0200

    feat: added link to cart and refined bootstrap

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,49 @@
+import { useContext } from 'react';
+import { Context } from '../Context';
+
 function Navbar() {
+  const { setPage } = useContext(Context);
+
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
         mangiaMangia
       </a>
       <button
         class="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="#">
+            <a
+              class="nav-link"
+              href="#"
+              onClick={() => {
+                setPage('home');
+              }}
+            >
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              href="#"
+              onClick={() => {
+                setPage('carrello');
+              }}
+            >
               Carrello
             </a>
           </li>
-          {/* <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li> */}
         </ul>
       </div>
     </nav>
