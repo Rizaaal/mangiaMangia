3c4d79c7bc1b97a7d1ef900a63f20a2088f60e1e
Gabriele Di Grazia <gabrii539@gmail.com>
2024-04-04 19:29:08 +0200

    feat: added CRUD calls to Card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,26 @@
-import { Component } from 'react';
+import { Component, useContext } from 'react';
+import { Context } from '../Context';
 
 class Card extends Component {
+  static contextType = Context;
+
   render() {
     return (
       <div className="col">
         <div className="card" style={{ width: '20rem', textAlign: 'center' }}>
           <button
             className="btn btn-primary"
+            onClick={() => {
+              this.context.addToCart(this.props.card);
+            }}
           >
             +1
           </button>
           <button
             className="btn btn-primary"
+            onClick={() => {
+              this.context.deleteFromCart(this.props.card.id);
+            }}
           >
             -1
           </button>
