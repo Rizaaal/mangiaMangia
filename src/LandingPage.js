ca80d60a2d7b5ed9a38d8f0d5e8cf9ea919fb8d2
Rita <rita.ciaramitaro@stevejobs.academy>
2024-04-04 23:32:21 +0200

    feat: adding landingPage

---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage({ setLandingPageVisible }) {
+  return (
+    <div>
+      <h1>Benvenuto nel nostro ristorante!</h1>
+      <button onClick={() => setLandingPageVisible(false)}>Entra nel sito</button>
+    </div>
+  );
+}
+
+export default LandingPage;
