8ea46fb73f1e4c0909c1a4146f844a914cadc45f
Gabriele Di Grazia <49413031+Rizaaal@users.noreply.github.com>
2024-04-04 20:27:31 +0200

    added complete cart functionality (#54)
    * feat: added Card component to cart page
    
    * feat: added CRUD calls to Card component
    
    * feat: made Card component reusable
    
    * chore: prettified files
    
    * refactor: addToCart and deleteFromCart
    
    removed getCart
    
    * fix: linked state to sessionStorage
    
    * feat: added pay, getCartTotal and getnumberOfCartItems
    
    * fix: set class to className appropriately
    
    * feat: added number of cart items display
    
    ---------
    
    Co-authored-by: Gabriele Di Grazia <gabrii539@gmail.com>

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,28 +1,40 @@
-import { Component } from 'react';
+import { Component, useContext } from 'react';
+import { Context } from '../Context';
 
 class Card extends Component {
+  static contextType = Context;
+
   render() {
     return (
       <div className="col">
         <div className="card" style={{ width: '20rem', textAlign: 'center' }}>
           <button
             className="btn btn-primary"
+            onClick={() => {
+              this.context.addToCart(this.props.card);
+            }}
           >
             +1
           </button>
           <button
             className="btn btn-primary"
+            onClick={() => {
+              this.context.deleteFromCart(this.props.card.id);
+            }}
           >
             -1
           </button>
           <img
             src={this.props.card.immagine}
             className="card-img-top"
-            alt="..."
+            alt="image not available"
           />
           <div className="card-body">
             <h5 className="card-title">{this.props.card.nome}</h5>
             <p className="card-text">{this.props.card.prezzo} euro</p>
+            {this.props.cart ? (
+              <p className="card-text">quantità: {this.props.card.quantity}</p>
+            ) : null}
           </div>
         </div>
       </div>
